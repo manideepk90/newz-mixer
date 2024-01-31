69fc7a025fb0c959479e3715798c67645653e6a0
Kuntimaddi Manideep <manideepk90@gmail.com>
2024-01-31 16:46:43 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import { createContext, useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useAuth } from "./lib/authContext";
 library.add(fas);
 import { authHeader } from "./lib/services/auth";
@@ -13,18 +13,20 @@ export default function Home() {
   const [data, setData] = useState([]);
   const page = useSearchParams().get("p");
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
   const [error, setError] = useState("");
   const { user, login } = useAuth();
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        console.log(authHeader())
+        console.log(authHeader());
         const res = await axios.get(`/api/articles?p=${page}`, {
           headers: authHeader(),
         });
         if (res?.data?.data) setData(() => res?.data?.data);
       } catch (err) {
+        if (err.response?.status === 401) router.push("/auth");
         setError("Failed to load Data");
       }
       setLoading(false);
