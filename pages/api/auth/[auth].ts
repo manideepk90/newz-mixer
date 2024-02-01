d7dfa494b6e65f9c17c79c0829e33271158189a1
Kuntimaddi Manideep <manideepk90@gmail.com>
2024-02-01 10:23:57 +0530

    Update [auth].ts

--- pages/api/auth/[auth].ts
+++ pages/api/auth/[auth].ts
@@ -9,60 +9,24 @@ export default async function handler(
 ) {
   const { auth } = req.query;
   const { body } = req;
-
-  switch (req.method) {
-    case "POST":
-      try {
-        if (auth === "login") {
-          const user = await prisma.user.findFirst({
-            where: {
-              email: body?.email,
-            },
-          });
-
-          if (!user) return res.status(403).json({ error: "No user found" });
-
-          if (await comparePasswords(body?.password, user?.password)) {
-            const payload = {
-              id: user.id,
-            };
-
-            const token = jwt.sign(payload, process.env.ACCESS_KEY_TOKEN, {
-              expiresIn: 215451,
-            });
-
-            return res.status(200).json({
-              success: true,
-              token: "Bearer " + token,
-              email: user.email,
-            });
-          } else {
-            return res.status(403).json({ error: "Invalid Credentials" });
-          }
-        } else if (auth === "signup") {
-          const user = await prisma.user.findFirst({
-            where: {
-              email: body?.email,
-            },
-          });
-
-          if (user) {
-            return res
-              .status(400)
-              .json({ error: "User already exists! Please login" });
-          }
-
-          if (body?.password?.length >= 8) {
-            const data = await prisma.user.create({
-              data: {
+  try {
+    switch (req.method) {
+      case "POST":
+        try {
+          if (auth === "login") {
+            const user = await prisma.user.findFirst({
+              where: {
                 email: body?.email,
-                password: await hashPassword(body?.password),
               },
             });
 
-            if (data) {
+            if (!user) {
+              return res.status(403).json({ error: "No user found" });
+            }
+
+            if (await comparePasswords(body?.password, user?.password)) {
               const payload = {
-                id: data.id,
+                id: user.id,
               };
 
               const token = jwt.sign(payload, process.env.ACCESS_KEY_TOKEN, {
@@ -72,24 +36,66 @@ export default async function handler(
               return res.status(200).json({
                 success: true,
                 token: "Bearer " + token,
-                email: data.email,
+                email: user.email,
               });
             } else {
-              return res.status(500).json({ error: "Failed to create user" });
+              return res.status(403).json({ error: "Invalid Credentials" });
             }
-          } else {
-            return res
-              .status(400)
-              .json({ error: "Enter a valid password (at least 8 characters)" });
           }
-        } else {
-          return res.status(400).json({ error: "No Api" });
+          //  else if (auth === "signup") {
+          //   const user = await prisma.user.findFirst({
+          //     where: {
+          //       email: body?.email,
+          //     },
+          //   });
+
+          //   if (user) {
+          //     return res
+          //       .status(400)
+          //       .json({ error: "User already exists! Please login" });
+          //   }
+
+          //   if (body?.password?.length >= 8) {
+          //     const data = await prisma.user.create({
+          //       data: {
+          //         email: body?.email,
+          //         password: await hashPassword(body?.password),
+          //       },
+          //     });
+
+          //     if (data) {
+          //       const payload = {
+          //         id: data.id,
+          //       };
+
+          //       const token = jwt.sign(payload, process.env.ACCESS_KEY_TOKEN, {
+          //         expiresIn: 215451,
+          //       });
+
+          //       return res.status(200).json({
+          //         success: true,
+          //         token: "Bearer " + token,
+          //         email: data.email,
+          //       });
+          //     } else {
+          //       return res.status(500).json({ error: "Failed to create user" });
+          //     }
+          //   } else {
+          //     return res.status(400).json({
+          //       error: "Enter a valid password (at least 8 characters)",
+          //     });
+          //   }
+          // } else {
+          //   return res.status(400).json({ error: "No Api" });
+          // }
+        } catch (error) {
+          console.error(error);
+          return res.status(500).json({ error: "Internal Server Error" });
         }
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-    default:
-      return res.status(400).json({ error: "API is not available" });
+      default:
+        return res.status(400).json({ error: "API is not available" });
+    }
+  } catch (err) {
+    console.log(err);
   }
 }
